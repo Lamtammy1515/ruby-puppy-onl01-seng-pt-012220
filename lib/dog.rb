1e64621ac4f583172627f5f950741e8f95f16456
Tammy Lam <lamtammy1515@gmail.com>
2020-02-07 01:17:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,19 @@ class Dog
   
   def initialize(name)
     @name = name 
+<<<<<<< HEAD
+=======
+    @@all << self
+>>>>>>> ae9b6143ddd82634ed665af3d27d5063c93c0d41
     save
   end 
   
   def self.all 
+<<<<<<< HEAD
     @@all
+=======
+    @@all.uniq!
+>>>>>>> ae9b6143ddd82634ed665af3d27d5063c93c0d41
   end 
   
   def self.clear_all 
